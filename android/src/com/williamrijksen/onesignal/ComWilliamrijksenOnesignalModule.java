fe097caed06af422b351a8b083856277829f6e4c
Felipe Freichos Godoy <ffelipegodoy@gmail.com>
2017-03-31 10:27:49 -0300

    adding getTags, idsAvailable, setSubscription functions to android module

--- android/src/com/williamrijksen/onesignal/ComWilliamrijksenOnesignalModule.java
+++ android/src/com/williamrijksen/onesignal/ComWilliamrijksenOnesignalModule.java
@@ -11,6 +11,7 @@ import java.util.HashMap;
 
 import org.appcelerator.kroll.KrollModule;
 import org.appcelerator.kroll.annotations.Kroll;
+import org.appcelerator.kroll.KrollFunction;
 import org.appcelerator.kroll.common.Log;
 import org.appcelerator.kroll.common.TiConfig;
 import org.appcelerator.titanium.TiApplication;
@@ -31,10 +32,14 @@ public class ComWilliamrijksenOnesignalModule extends KrollModule
 		.startInit(appContext)
 		.setNotificationReceivedHandler(new NotificationReceivedHandler())
 		.setNotificationOpenedHandler(new NotificationOpenedHandler())
-		.inFocusDisplaying(OneSignal.OSInFocusDisplayOption.None)
+		.inFocusDisplaying(OneSignal.OSInFocusDisplayOption.Notification)
 		.init();
 	}
 	//TODO inFocusDisplaying should be configurable from Titanium App module initialization
+	
+	//variable to store the received call back function for the getTags method call
+	private KrollFunction getTagsCallback = null;
+	private KrollFunction idsAvailableCallback = null;
 
 	@Kroll.onAppCreate
 	public static void onAppCreate(TiApplication app)
@@ -58,6 +63,59 @@ public class ComWilliamrijksenOnesignalModule extends KrollModule
 		String key = TiConvert.toString(dict, "key");
 		OneSignal.deleteTag(key);
 	}
+	
+	@Kroll.method
+	public void getTags(KrollFunction handler)
+	{
+		getTagsCallback = handler;
+		OneSignal.getTags(new GetTagsHandler());
+	}
+	
+	@Kroll.method
+	public void idsAvailable(KrollFunction handler)
+	{
+		idsAvailableCallback = handler;
+		OneSignal.idsAvailable(new IdsAvailableHandler());
+	}
+	
+	@Kroll.method
+	public void setSubscription(Boolean subscription)
+	{
+		OneSignal.setSubscription(subscription);
+	}
+	
+	private class IdsAvailableHandler implements OneSignal.IdsAvailableHandler {
+		@Override
+		public void idsAvailable(String userId, String registrationId) {
+			HashMap dict = new HashMap();
+			if(userId != null){
+				dict.put("userId", userId);
+			}
+			if (registrationId != null){
+			  dict.put("registrationId", registrationId);
+			}
+			idsAvailableCallback.call(getKrollObject(), dict);
+		}
+	}
+	
+	private class GetTagsHandler implements OneSignal.GetTagsHandler {
+		@Override
+		public void tagsAvailable(JSONObject tags) {
+			HashMap dict = new HashMap();
+			try {
+				dict.put("success", true);
+				dict.put("error", false);
+				dict.put("results", tags.toString());
+			} catch (Exception e) {
+				dict.put("success", false);
+				dict.put("error", true);
+				e.printStackTrace();
+				Log.d("error:", e.toString());
+			}
+
+			getTagsCallback.call(getKrollObject(), dict);
+		}
+	}
 
 	private class NotificationOpenedHandler implements OneSignal.NotificationOpenedHandler {
 		// This fires when a notification is opened by tapping on it.
@@ -67,7 +125,7 @@ public class ComWilliamrijksenOnesignalModule extends KrollModule
 			String body = result.notification.payload.body;
 			JSONObject additionalData = result.notification.payload.additionalData;
 
-			HashMap<String, Object> kd = new HashMap<String, Object>();
+			HashMap kd = new HashMap();
 			if(title != null){
 				kd.put("title", title);
 			}
@@ -90,7 +148,7 @@ public class ComWilliamrijksenOnesignalModule extends KrollModule
 			JSONObject additionalData = notification.payload.additionalData;
 			if(additionalData != null){
 				String payload = additionalData.toString();
-				HashMap<String, Object> kd = new HashMap<String, Object>();
+				HashMap kd = new HashMap();
 				kd.put("additionalData", payload);
 				fireEvent("notificationReceived", kd);
 			}else{
